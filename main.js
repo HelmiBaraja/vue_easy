8a719919033e611d0044754fe2f862f93279ccab
Helmi Baraja <helmi.baraja@gmail.com>
2020-10-05 17:42:40 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <button>ON</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        items: null
+      };
+    }
+  };
+</script>
